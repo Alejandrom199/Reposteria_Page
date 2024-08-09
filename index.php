9da2a5e4c2bdb47811ffed76e33335385ad18c0d
Saúl Maldonado lópez <Alejandrom199916@gmail.com>
2024-08-09 09:29:55 -0500

    cambio a php

---
+++ index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="description" content="Página de inicio de la pastelería.">
+        <meta name="keywords" content="pastelería, dulces, repostería">
+        <meta name="author" content="Saul Maldonado López">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Pastelería CupCake - Inicio</title>
+        <link rel="stylesheet" href="./styles/index-style.css">
+    </head>
+
+    <body>
+        <!--Header-->
+
+        <!--Main-->
+        <main >
+            <div class="maquetacion-main">
+                <div>
+                    <div class="img-principal">
+                        <div class="contenedor-titulos">
+                            <h1 class="titulo-principal">Pastelería CupCake</h1>
+                            <h2 class="titulo-secundario">Descubre la Magia de la Repostería</h2>
+                        </div>
+                    </div>
+                </div>
+    
+                <div class="contenedor-section">
+                    <section>
+                        <div class="contenedor-novedades">
+                            <h2 class="titulo-novedades">Novedades</h2>
+                        </div>
+                        <section>
+                            
+                            <h3>Ultimas recetas Agregadas</h3>
+                            <div class="contenedor-novedades-int">
+                                
+                                <div class="contenedor-recetas">
+                                    <h4>Receta #1</h4>
+                                    <div class="contenedor-img-p">
+                                        <img class="img-postre" src="img/dulce.jpg" alt="postre">
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, ipsa. Doloremque excepturi ducimus ad repudiandae nostrum blanditiis aspernatur ratione minima voluptatem nihil! Minus eum rerum, dicta delectus ad ipsam esse.</p>
+                                    </div>
+                                </div>
+    
+                                <div class="contenedor-recetas"> 
+                                    <h4>Receta #2</h4>
+                                    <div class="contenedor-img-p">
+                                        <img class="img-postre" src="img/dulce.jpg" alt="postre">
+                                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis architecto libero natus enim ea velit ipsum ullam incidunt repudiandae totam distinctio suscipit, esse ratione! Vitae magni eum deserunt officia voluptatum</p>
+                                    </div>
+                                </div>
+    
+                                <div class="contenedor-recetas">
+                                    <h4>Receta #3</h4>
+                                    <div class="contenedor-img-p">
+                                        <img class="img-postre" src="img/dulce.jpg" alt="postre">
+                                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. In reiciendis laborum nam voluptate aut quas a fugit exercitationem quasi facilis quis ullam sit perferendis, repellendus aliquid earum suscipit omnis velit.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                        
+                        <br>
+                        
+                        <section>
+                            <h3>Lista de Recetas populares</h3>
+                            <div>
+                                <ol>
+                                    <li>Receta popular #1</li>
+                                    <li>Receta popular #2</li>
+                                    <li>Receta popular #3</li>
+                                    <li>Receta popular #4</li>
+                                </ol>
+                            </div>
+                        </section>
+                        <br>
+                
+                        <section>
+                            <h3>Contenedor de Testimonios</h3>
+                            <div class="contenedor-novedades-int">
+                                <div class="contenedor-clientes">
+                                    <h4>Cliente #1</h4>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur maiores quasi totam repellat minima corporis voluptatum! Quasi nisi, exercitationem neque molestias quas debitis, vitae provident possimus, quia impedit eligendi fuga?</p>
+                                </div>
+    
+                                <div class="contenedor-clientes">
+                                    <h4>Cliente #2</h4>
+                                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero, at reiciendis! Corporis numquam architecto velit tempore, quam repudiandae dolores cumque molestiae porro magnam ea et adipisci, error consectetur beatae nam.</p>
+                                </div>
+    
+                                <div class="contenedor-clientes">
+                                    <h4>Cliente #3</h4>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere debitis facilis magnam laborum mollitia eaque necessitatibus voluptate eius eum alias atque dignissimos dolorum, iusto ipsum iste eveniet exercitationem rerum placeat.</p>
+                                </div>
+    
+                                <div class="contenedor-clientes">
+                                    <h4>Cliente #4</h4>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero blanditiis voluptate ipsam necessitatibus dolores dicta. Veniam, totam. Repudiandae in ipsum dolorem magni assumenda! Blanditiis veritatis eius dolorum natus, quo ab!</p>
+                                </div>
+                            </div>
+                        </section>
+                    </section>
+                </div>
+            </div>
+        </main>
+        <br>
+         <!--Footer-->
+         <?php include("footer.html");?>
+    </body>
+</html>
